a14362029292fe0f58b4419def923bb3bdaface0
Ansh Thakur <mailtodroidoox@gmail.com>
2026-01-13 23:16:50 +0530

    401 error fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+// Set up Axios interceptor to include token in all requests
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
